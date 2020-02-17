a29db1e0a0b30ad3da70803d40c0feb4ac40864c
P3-NSF-NRT <60619020+P3-NSF-NRT@users.noreply.github.com>
2020-02-16 18:03:27 -0600

    Update takephoto.py

--- Camera Activity/code/takephoto.py
+++ Camera Activity/code/takephoto.py
@@ -3,14 +3,11 @@ import time
 import sys
 
 camera = picamera.PiCamera()
-#delay = 3
-#input_name = "test"
+
 delay = int(sys.argv[1])
 input_name = sys.argv[2]
 time.sleep(delay)
-#camera.capture(output_file)
-#camera.start_preview()
-#camera.capture("test2.jpg")
+
 camera.capture("photos/%s.jpg" % input_name)
-#camera.stop_preview()
+
 
